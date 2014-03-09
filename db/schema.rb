0fc2eba26c191229ecc9bce86410a02cd41c77e7
Edilson Gonzalez <edilson.gonza@unitec.edu>
2014-03-09 03:07:29 +0000

    Escribiendo texto pagina principal

--- db/schema.rb
+++ db/schema.rb
@@ -60,15 +60,6 @@ ActiveRecord::Schema.define(version: 20140304191413) do
     t.datetime "updated_at"
   end
 
-  create_table "customers", force: true do |t|
-    t.string   "name"
-    t.string   "phone"
-    t.text     "address"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "package_types", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
